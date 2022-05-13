b1d4727f86da13a0c75b5ec8d5f166fd9e274d3a
mehmetozk4n6 <81770287+mehmetozk4n6@users.noreply.github.com>
2022-05-13 22:43:07 +0300

    ContactApp created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Contacts from "./components/Contacts";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Edit from "./components/Contacts/Edit";
+import Error from "./components/Contacts/Error";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Contacts />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
